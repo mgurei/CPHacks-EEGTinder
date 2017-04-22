03dfd29f1b90b7710564e97efdd165e2abd8bed1
mgurei <gurei.mihai@gmail.com>
2017-04-22 17:58:08 +0200

    Android Studio project

---
+++ Test/app/src/main/java/com/copenhacks2017/eegtinder/MainActivity.java
@@ -0,0 +1,13 @@
+package com.copenhacks2017.eegtinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
